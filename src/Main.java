8f94a859462bddf4867b25b88bb200880e707727
ZU <zshaihova@gmail.com>
2023-04-20 14:21:23 +0300

    задачи 1-5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
     public class Main {
         public static void main(String[] args) {
-            task1(); task2(); task3();//task4();task5();task6();task7();task8();
+            task1(); task2(); task3();task4();task5();//task6();task7();task8();
         }
     public static void task1 () {
         System.out.println("Задача 1:");
@@ -37,5 +37,26 @@
             countryY += Math.abs((countryY / 1000 * fertility) - (countryY / 1000 * mortality));
             System.out.println("год " + i + "," + "численность населения составляет " + countryY);
         }
+    }
+
+    public static void task4 () {
+            System.out.println("Задача 4:");
+            int deposit = 15_000;
+            for (int month =1; deposit <= 12_000_000; month++) {
+                deposit *= 1.07;
+                System.out.println("месяц " + month + ", накоплений " + deposit);
+            }
+     }
+
+    public static void task5 () {
+            System.out.println("Задача 5:");
+            int deposit = 15_000;
+            for (int month =1; deposit <= 12_000_000; month++) {
+                deposit *= 1.07;
+                if (month % 6 ==0)
+                System.out.println("месяц " + month + ", накоплений " + deposit);
+            }
+
+
     }
 }
