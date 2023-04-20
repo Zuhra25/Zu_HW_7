aa41325b6397189a4598dce85eb4b1cb8d6a9f8d
ZU <zshaihova@gmail.com>
2023-04-20 12:41:01 +0300

    задача 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,41 @@
 
     public class Main {
         public static void main(String[] args) {
-            task1(); //task2();task3();task4();task5();task6();task7();task8();
+            task1(); task2(); task3();//task4();task5();task6();task7();task8();
         }
+    public static void task1 () {
+        System.out.println("Задача 1:");
+        int invest = 15_000;
+        int deposit = 0;
+        int month = 0;
+        while (deposit <= 2_459_000) {
+            deposit += invest;
+            month += 1;
+            System.out.println("Месяц " + month + "," + " сумма накоплений равна " + deposit + " рублеймесяц ");
+        }
+    }
 
-        public static void task1 () {
-            System.out.println("Задача 1");
-            int invest = 15_000;
-            int deposit = 0;
-            int month = 0;
-            while (deposit <= 2_459_000) {
-                deposit += invest;
-                month += 1;
-                System.out.println("Месяц " + month + "," + " сумма накоплений равна " + deposit + " рублеймесяц ");
+    public static void task2 () {
+            System.out.println("Задача 2:");
+            int number = 1;
+            while (number <= 10){
+                System.out.print(number + " ");
+                number += 1;
             }
+            System.out.println();
+            for (int i = 10; i >= 1; i--) {
+                System.out.print(i + " ");
+            }
+        System.out.println();
+    }
+    public static void task3 () {
+        System.out.println("Задача 3:");
+        int countryY = 12_000_000;
+        int fertility = 17;
+        int mortality = 8;
+        for (int i = 1; i <= 10; i++) {
+            countryY += Math.abs((countryY / 1000 * fertility) - (countryY / 1000 * mortality));
+            System.out.println("год " + i + "," + "численность населения составляет " + countryY);
         }
-
-//
-//        public static void task2 () {
-//            System.out.println("Задача 2");
-//        ... // Пишем код для задачи 2
-//        }
+    }
 }
