06cc095dfb5384ce1cef9c7fecc7dc51fdb05068
ZU <zshaihova@gmail.com>
2023-04-20 11:02:16 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
 
+    public class Main {
+        public static void main(String[] args) {
+            task1(); //task2();task3();task4();task5();task6();task7();task8();
+        }
 
+        public static void task1 () {
+            System.out.println("Задача 1");
+            int invest = 15_000;
+            int deposit = 0;
+            int month = 0;
+            while (deposit <= 2_459_000) {
+                deposit += invest;
+                month += 1;
+                System.out.println("Месяц " + month + "," + " сумма накоплений равна " + deposit + " рублеймесяц ");
+            }
+        }
 
+//
+//        public static void task2 () {
+//            System.out.println("Задача 2");
+//        ... // Пишем код для задачи 2
+//        }
 }
